62dcf9c782a15eb3d7a106ee9f8f0ecba8f25d7e
godshadows <78544333+godshadows@users.noreply.github.com>
2021-02-07 12:31:13 +0800

    Update migu.sh

--- migu.sh
+++ migu.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash 
 
 echo '------------------sign------------------'
-curl -H "Cookie: ${COOKIE}" -X POST 'https://godshadows.com/api/Leav/gethq'
+curl -H "Cookie: ${COOKIE}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/sign'
 echo ''
 echo '---------------check sign---------------'
 curl -H "Cookie: ${COOKIE}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/checkSign'
+echo ''
+echo '---------------check sign---------------'
+curl -H "Cookie: ${COOKIE}" -X POST 'https://godshadows.com/api/Leav/ttttt'
