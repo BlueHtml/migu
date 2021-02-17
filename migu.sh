4fe8fcd1090fa16f61f3c70e66c65727f51e0019
godshadows <78544333+godshadows@users.noreply.github.com>
2021-02-17 10:05:41 +0800

    Update migu.sh

--- migu.sh
+++ migu.sh
@@ -8,5 +8,3 @@ curl -H "Cookie: ${COOKIE}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/disp
 echo ''
 echo '---------------lottery---------------'
 curl -H "Content-Type: application/json; charset=UTF-8" -H "Cookie: ${COOKIE}" -d '{}' -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/activity/call-charge/user/lottery'
-echo '---------------test---------------'
-curl -H "Content-Type: application/json; charset=UTF-8" -H "Cookie: ${COOKIE}" -d '{"channel":"0003","payWay":"WAP","amount":99.8,"chargeMoney":100,"choseMoney":100,"activityNO":"","operateId":"3215","homeProv":"","numFlag":"0","source":""}' -X POST 'https://touch.10086.cn/i/v1/pay/saveorder/15168709005?provinceId=571'
